c5bfb8247c0fe30553a15d3c20c4102bef8173c4
kalumwe <113726686+kalumwe@users.noreply.github.com>
2023-10-26 22:50:50 +0200

    initial commit

---
+++ src/app/components/CalendarEntry.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="calendar-entry">
+    <!-- Markup for calendar entry -->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CalendarEntry'
+  }
+</script>
+
+<style lang="scss" scoped>
+    #calendar-entry {
+
+    }
+</style>
